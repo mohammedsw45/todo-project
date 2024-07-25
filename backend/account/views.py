0af7788cad86edfbccbb3bd50fc4bd1a35bf4204
mohammedsw45 <mohammedabduallah95@outlook.com>
2024-07-25 11:36:10 +0300

    Edit Staffing

--- backend/account/views.py
+++ backend/account/views.py
@@ -53,6 +53,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
 
         token['username'] = user.username
+	token['is_staff'] = user.is_staff
 
         return token
     
